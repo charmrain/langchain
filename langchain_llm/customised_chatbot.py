02fd05da17517bde1efb2a68cd50c0077d2eb195
raymond <raymondan1031@gmail.com>
2023-08-14 11:50:25 -0500

    start the process

---
+++ langchain_llm/customised_chatbot.py
@@ -0,0 +1,51 @@
+import os
+import sys
+
+import openai
+from langchain.chains import ConversationalRetrievalChain, RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import DirectoryLoader, TextLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.indexes.vectorstore import VectorStoreIndexWrapper
+from langchain.llms import OpenAI
+from langchain.vectorstores import Chroma
+
+
+
+def data_ingress():
+    """ this compenent aims to incorprate multiple pdf files
+    those files can provide the basic information to chatbot,
+    when the chatbox answer any questions, it will use the information 
+    that given by the pdf files
+
+    input: pdf.files
+    output: an object that langchain can digest
+    """
+    pass
+
+
+def langchain_chatbot():
+    """ this compenent is to implement Langchain to train a chatbot with the pdf files
+    the chatbot is to digest the knowledge shared by the data_ingress function
+
+    input: data_ingress()
+    output: langchain chatbox
+    """
+    pass
+
+
+
+def evaluation():
+    """ this compenent aims to find out the quality of the chatbox
+    the question that we know the answer will pitch to the chatbot,
+    see how many questions the chatbot can answer correctly
+
+    input: questions for the chatbot
+    output: the accuracy rate and error answer
+    """
+    pass
+
+
+if __name__ == '__main__':
+    pass
